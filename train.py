78801b1cfd807adba408cfd4e7cd70b99136a514
Prakash <prakashkmuthu@gmail.com>
2026-06-07 18:57:35 +0530

    Train Decision Tree on dataset V2

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 # Read dataset
 df = pd.read_csv("data/demo.csv")
@@ -25,32 +26,25 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Start MLflow experiment
 mlflow.set_experiment("student_pass_fail_experiment")
 
-with mlflow.start_run(run_name="Logistic Regression - Dataset V1"):
+with mlflow.start_run(run_name="Decision Tree - Dataset V2"):
 
-    # Model
-    model = LogisticRegression()
+    model = DecisionTreeClassifier(random_state=42)
 
-    # Train
     model.fit(X_train, y_train)
 
-    # Predict
     y_pred = model.predict(X_test)
 
-    # Accuracy
     accuracy = accuracy_score(y_test, y_pred)
 
     print("Accuracy:", accuracy)
 
-    # Log information to MLflow
-    mlflow.log_param("model_name", "Logistic Regression")
-    mlflow.log_param("dataset_version", "V1")
+    mlflow.log_param("model_name", "Decision Tree")
+    mlflow.log_param("dataset_version", "V2")
     mlflow.log_param("test_size", 0.2)
     mlflow.log_param("random_state", 42)
 
     mlflow.log_metric("accuracy", accuracy)
 
-    # Save model locally
     joblib.dump(model, "models/model.pkl")
 
-    # Log model in MLflow
     mlflow.sklearn.log_model(model, "model")
